f87aac68ae5a92a8fa1319601c52d9adedd6a09f
Fisayo1770 <65889376+Fisayo1770@users.noreply.github.com>
2023-07-22 05:01:02 +0100

    comp

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ app.use("/api/users", userRoutes);
 app.use("/api/page", pageRoutes)
 
 
-  // app.use(express.static(path.join(__dirname, "./client/build")));
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "./client/build", "index.html"));
-  // });
+  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+  });
 
 const port = process.env.PORT;
 app.listen(port, () => {
